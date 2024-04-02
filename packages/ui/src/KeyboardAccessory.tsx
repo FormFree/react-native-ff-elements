57d72d096fc2d54f3cd082a7679d31de387f8601
Andy Phillipson <andyphillipson@Andys-MacBook-Pro.local>
2024-04-02 12:20:30 -0400

    Update keyboard accessory view

--- packages/ui/src/KeyboardAccessory.tsx
+++ packages/ui/src/KeyboardAccessory.tsx
@@ -1,30 +1,98 @@
-import { AppTheme, useTheme, viewport } from './theme';
+import { AppTheme, useTheme } from './theme';
 import { Button, Icon } from '@rneui/base';
-import { InputAccessoryView, Keyboard, View } from 'react-native';
+import { InputAccessoryView, Keyboard, TextInput, View } from 'react-native';
 
-import React from 'react';
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
 import { makeStyles } from '@rneui/themed';
+import { viewport } from '@react-native-ff-elements/ui';
 
-interface KeyboardAccessory {
-  nextDisabled?: boolean;
+type AccessoryState = {
+  fieldCount: number;
+  focusedField: number;
+  nextDisabled: boolean;
+  previousDisabled: boolean;
+};
+
+export interface KeyboardAccessory {
+  // The fieldRefs array must be ordered the same as intended field navigation on the form.
+  fieldRefs: (TextInput | null)[];
+  id: string;
   onDone?: () => void;
-  onNext: () => void;
-  onPrevious: () => void;
-  previousDisabled?: boolean;
 }
 
-const KeyboardAccessory = ({
-  nextDisabled,
-  onDone,
-  onNext,
-  onPrevious,
-  previousDisabled,
-}: KeyboardAccessory) => {
+export interface KeyboardAccessorylMethods {
+  focusedField: (field: number) => void;
+}
+
+const KeyboardAccessory = forwardRef<
+  KeyboardAccessorylMethods,
+  KeyboardAccessory
+>(({ fieldRefs, id, onDone }: KeyboardAccessory, ref) => {
   const theme = useTheme();
   const s = useStyles(theme);
 
+  useImperativeHandle(ref, () => ({
+    //  These functions exposed to the parent component through the ref.
+    focusedField,
+  }));
+
+  const [editorState, setEditorState] = useState<AccessoryState>({
+    fieldCount: fieldRefs.length,
+    focusedField: 0,
+    nextDisabled: fieldRefs.length === 0,
+    previousDisabled: true,
+  });
+  console.log(editorState);
+
+  const focusedField = (field: number) => {
+    const nextDisabled = field === fieldRefs.length - 1;
+    const previousDisabled = field === 0;
+    setEditorState(prevState => {
+      return {
+        ...prevState,
+        focusedField: field,
+        nextDisabled,
+        previousDisabled,
+      };
+    });
+  };
+
+  const next = () => {
+    if (editorState.focusedField === undefined) return;
+    const nextField = editorState.focusedField + 1;
+    const nextDisabled = nextField === fieldRefs.length - 1;
+    const previousDisabled = nextField === 0;
+    console.log(nextField, nextDisabled);
+    fieldRefs[nextField]?.focus();
+    setEditorState(prevState => {
+      return {
+        ...prevState,
+        focusedField: nextField,
+        nextDisabled,
+        previousDisabled,
+      };
+    });
+  };
+
+  const previous = () => {
+    if (editorState.focusedField === undefined) return;
+    const nextField = editorState.focusedField - 1;
+    console.log('nextField', nextField);
+    const nextDisabled = nextField === fieldRefs.length;
+    const previousDisabled = nextField === 0;
+    fieldRefs[nextField]?.focus();
+    setEditorState(prevState => {
+      return {
+        ...prevState,
+        nextField,
+        nextDisabled,
+        previousDisabled,
+      };
+    });
+  };
+
   return (
-    <InputAccessoryView nativeID={'inputAccessoryViewID'}>
+    <InputAccessoryView nativeID={id}>
       <View style={s.container}>
         <Button
           type={'clear'}
@@ -34,15 +102,12 @@ const KeyboardAccessory = ({
               type={'ionicon'}
               color={theme.colors.screenHeaderButtonText}
               size={28}
-              disabled={previousDisabled}
-              disabledStyle={{
-                backgroundColor: theme.colors.transparent,
-                opacity: 0.3,
-              }}
+              disabled={editorState.previousDisabled}
+              disabledStyle={s.fieldNavButton}
             />
           }
-          disabled={previousDisabled}
-          onPress={onPrevious}
+          disabled={editorState.previousDisabled}
+          onPress={previous}
         />
         <Button
           type={'clear'}
@@ -52,15 +117,12 @@ const KeyboardAccessory = ({
               type={'ionicon'}
               color={theme.colors.screenHeaderButtonText}
               size={28}
-              disabled={nextDisabled}
-              disabledStyle={{
-                backgroundColor: theme.colors.transparent,
-                opacity: 0.3,
-              }}
+              disabled={editorState.nextDisabled}
+              disabledStyle={s.fieldNavButton}
             />
           }
-          disabled={nextDisabled}
-          onPress={onNext}
+          disabled={editorState.nextDisabled}
+          onPress={next}
         />
         <Button
           type={'clear'}
@@ -72,7 +134,7 @@ const KeyboardAccessory = ({
       </View>
     </InputAccessoryView>
   );
-};
+});
 
 const useStyles = makeStyles((_theme, theme: AppTheme) => ({
   container: {
@@ -91,6 +153,10 @@ const useStyles = makeStyles((_theme, theme: AppTheme) => ({
     ...theme.styles.textNormal,
     ...theme.styles.textBold,
     color: theme.colors.screenHeaderButtonText,
+  },
+  fieldNavButton: {
+    backgroundColor: theme.colors.transparent,
+    opacity: 0.3,
   },
 }));
 
