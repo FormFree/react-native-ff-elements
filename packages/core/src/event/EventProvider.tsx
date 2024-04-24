0f71e807493581770136ed4bf46c90f9faeef868
Andy Phillipson <andyphillipson@Andys-MBP.attlocal.net>
2024-04-24 17:00:28 -0400

    Add useEvent hook

---
+++ packages/core/src/event/EventProvider.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode, createContext } from 'react';
+
+import EventEmitter from 'eventemitter3';
+
+const event = new EventEmitter();
+export const EventContext = createContext<EventEmitter>(event);
+
+export const EventProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}): ReactNode => {
+  return (
+    <EventContext.Provider value={event}>{children}</EventContext.Provider>
+  );
+};
