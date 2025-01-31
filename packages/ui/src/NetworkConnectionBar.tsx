cbfea1e9bdfc61d77efce570e18c25549a1e4eee
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2025-01-31 15:49:48 -0500

    Network connection bar params

--- packages/ui/src/NetworkConnectionBar.tsx
+++ packages/ui/src/NetworkConnectionBar.tsx
@@ -1,21 +1,26 @@
 import { useContext, useEffect } from 'react';
 
-import Toast from 'react-native-toast-message';
+import Toast, { type ToastShowParams } from 'react-native-toast-message';
 import { NetworkContext } from '@react-native-ff-elements/core';
 
-const NetworkConnectionBar = () => {
+interface Props {
+  toastProps: ToastShowParams;
+}
+
+const NetworkConnectionBar = ({ toastProps }: Props) => {
   const network = useContext(NetworkContext);
 
   useEffect(() => {
     if (!network.state?.isConnected) {
       Toast.show({
-        type: 'networkToast',
+        type: 'networkConnection',
         text1: 'No internet connection',
         text2: 'Check your internet connection and try again.',
         position: 'top',
         visibilityTime: 5000,
         autoHide: false,
         topOffset: 70,
+        ...toastProps,
       });
     } else {
       Toast.hide();
