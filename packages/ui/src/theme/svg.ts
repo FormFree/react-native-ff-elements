be49bf8c8c494b6843ff9a7c5acd08d4286731e3
Andy Phillipson <1063765+ajp8164@users.noreply.github.com>
2025-03-08 12:25:00 -0500

    Add optional fill/stroke to svg

--- packages/ui/src/theme/svg.ts
+++ packages/ui/src/theme/svg.ts
@@ -2,12 +2,20 @@ import { FormFreeElements } from '@react-native-ff-elements/core';
 import type { SVGImages } from '@react-native-ff-elements/common';
 import iconMissing from './img/icon-missing.svg';
 
-export const getColoredSvg = (which: string) => {
+type SvgOptions = {
+  fill: boolean;
+  stroke: boolean;
+};
+
+export const getColoredSvg = (which: string, opts?: SvgOptions) => {
+  const fill = opts?.fill === undefined ? true : false;
+  const stroke = opts?.stroke === undefined ? true : false;
+
   // Replace fill and stroke colors with 'currentColor' literal.
   // Allows react-native-svg to replace the color using style color property.
   let i = getSvg(which);
-  i = i.replace(/(s|fill)="(.[0-9a-fA-F]*)"/g, 'fill="currentColor"');
-  i = i.replace(/(s|stroke)="(.[0-9a-fA-F]*)"/g, 'stroke="currentColor"');
+  i = fill ? i.replace(/(s|fill)="(.[0-9a-fA-F]*)"/g, 'fill="currentColor"') : i;
+  i = stroke ? i.replace(/(s|stroke)="(.[0-9a-fA-F]*)"/g, 'stroke="currentColor"') : i;
   return i;
 };
 
